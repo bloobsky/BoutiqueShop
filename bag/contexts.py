d1b80a2a06c85560f179c725f75c93cf938684e9
bloobsky <bloobsky@gmail.com>
2020-07-11 09:42:02 +0000

    Add Products pt.1

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 from django.conf import settings
+
 def bag_contents(request):
 
     bag_items = []
@@ -12,9 +13,9 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-
-    grand_total = delivery + total 
-
+    
+    grand_total = delivery + total
+    
     context = {
         'bag_items': bag_items,
         'total': total,
